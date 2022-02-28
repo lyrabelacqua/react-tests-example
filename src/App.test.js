42fcba997ba5a02e4779d1fb5bb91d28e440ea64
Monika Klokosinska <monika.klokosinska@gmail.com>
2022-02-28 20:16:51 +0100

    feat: search (not really)

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,6 @@ test('renders learn react link', () => {
   render(<App />);
   //const linkElement = screen.getByText(/learn react/i);
   const linkElement = screen.getByRole('link', { text: /learn react/})
+  console.log('i will fix this later');
   expect(linkElement).toBeInTheDocument();
 });
